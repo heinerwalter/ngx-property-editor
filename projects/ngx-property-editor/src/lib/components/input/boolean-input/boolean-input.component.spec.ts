64af0ffbe81af89ce4560ff584489ea9c1b416ed
Heiner Walter <heiner.walter@tu-dortmund.de>
2023-10-30 20:12:33 +0100

    fixed tests

--- projects/ngx-property-editor/src/lib/components/input/boolean-input/boolean-input.component.spec.ts
+++ projects/ngx-property-editor/src/lib/components/input/boolean-input/boolean-input.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BooleanInputComponent } from './boolean-input.component';
 import { FormGroupComponent } from '../form-group/form-group.component';
 import { RadioInputComponent } from '../radio-input/radio-input.component';
+import { SingleRadioInputComponent } from '../single-radio-input/single-radio-input.component';
 import { FormsModule } from '@angular/forms';
 
 describe('BooleanInputComponent', () => {
@@ -15,6 +16,7 @@ describe('BooleanInputComponent', () => {
         BooleanInputComponent,
         FormGroupComponent,
         RadioInputComponent,
+        SingleRadioInputComponent, 
       ],
       imports: [ FormsModule ],
     });
