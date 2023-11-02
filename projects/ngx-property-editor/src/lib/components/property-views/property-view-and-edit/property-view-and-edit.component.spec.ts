b4d1ebc7480ecbd32006ea6c96cc894cd4f88b52
heinerwalter <heinerwalter@users.noreply.github.com>
2023-11-03 00:53:28 +0100

    fixed test

--- projects/ngx-property-editor/src/lib/components/property-views/property-view-and-edit/property-view-and-edit.component.spec.ts
+++ projects/ngx-property-editor/src/lib/components/property-views/property-view-and-edit/property-view-and-edit.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PropertyViewAndEditComponent } from './property-view-and-edit.component';
 import { PropertyEditorModule } from '../../../property-editor.module';
 
 describe('PropertyViewAndEditComponent', () => {
