c10629089dd9375280a1d6de5f6b8a0ead7b0f47
Heiner Walter <heiner.walter@tu-dortmund.de>
2023-11-05 14:54:04 +0100

    fixed test

--- projects/ngx-property-editor/src/lib/components/bootstrap/item-views/tabs-item/tabs-item.component.spec.ts
+++ projects/ngx-property-editor/src/lib/components/bootstrap/item-views/tabs-item/tabs-item.component.spec.ts
@@ -7,16 +7,17 @@ describe('TabsItemComponent', () => {
   let fixture: ComponentFixture<TabsItemComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TabsItemComponent]
-    });
-    fixture = TestBed.createComponent(TabsItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should not create', () => {
     // Item cannot be created outside of the items view container
-    expect(component).toThrow(new Error("No provider for TabsComponent!"));
+    expect(() => {
+      TestBed.configureTestingModule({
+        declarations: [TabsItemComponent]
+      });
+      fixture = TestBed.createComponent(TabsItemComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }).toThrow(new Error("No provider for TabsComponent!"));
   });
 });
