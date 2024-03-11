39e5262ce9e252cb3006227be4b0ebbd643998b4
heinerwalter <heiner.walter@tu-dortmund.de>
2024-03-11 13:15:27 +0100

    Update icon-select-input.component.ts

--- projects/ngx-property-editor/src/lib/components/input/special-input/icon-select-input/icon-select-input.component.ts
+++ projects/ngx-property-editor/src/lib/components/input/special-input/icon-select-input/icon-select-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InputBaseWithValue } from '../../input-base';
-import { iconDataSource } from './fa-icon-data-source';
+import { getIconDataSource } from './fa-icon-data-source';
 
 /**
  * This component provides a simplified select field for selecting icon classes.
@@ -12,47 +12,35 @@ import { iconDataSource } from './fa-icon-data-source';
   templateUrl: './icon-select-input.component.html',
   styleUrls: ['./icon-select-input.component.scss'],
 })
-export class IconSelectInputComponent extends InputBaseWithValue<string> implements OnChanges {
+export class IconSelectInputComponent extends InputBaseWithValue<string> {
 
   /** If true, an additional empty item is added to enable empty selection (`value == undefined`). */
   @Input() public allowEmpty: boolean = false;
 
   /**
-   * An array of selectable icon classes.
+   * An array of selectable icons.
+   * The data source items must contain at least the following properties:
+   * - `label`: A human-readable label which is interpreted as `[innerHTML]` of the select option elements.
+   * - `value`: The value which is emitted via `(valueChange)` when the user selected the item.
    */
-  @Input() public iconDataSource: string[] = iconDataSource;
+  @Input() public dataSource: ({ label: string, value: string } & any)[] = [];
 
   /**
    * An optional prefix added to the icons from the `iconDataSource` property
-   * to create a CSS class which will be added to the select items.
+   * to create a CSS class which will be added to the default select items.
    */
-  @Input() iconClassPrefix: string | undefined = 'fa-solid fa-';
-
-  /**
-   * An array from which the user can select one or multiple items.
-   */
-  protected dataSource: any[] = [];
+  protected readonly iconClassPrefix: string | undefined = ''; //'fa-solid fa-';
 
   public constructor() {
     super();
-    this.updateDataSource();
-  }
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes.hasOwnProperty('iconDataSource')) {
-      this.updateDataSource();
-    }
+    this.assignDefaultDataSource();
   }
 
   /**
-   * This function updates the `dataSource` property from the `iconDataSource`.
+   * This function assigns all free solid FontAwesome icons to the `dataSource` property.
    */
-  private updateDataSource(): void {
-    this.dataSource = this.iconDataSource
-      .map(icon => ({
-        icon: icon,
-        class: (this.iconClassPrefix || '') + icon,
-      }));
+  private assignDefaultDataSource(): void {
+    this.dataSource = this.getIconDataSource(this.iconClassPrefix, true);
   }
 
 }
