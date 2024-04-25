4bdfd3883f79a503d5f89a95307176fb725770f6
Heiner Walter <heiner.walter@tu-dortmund.de>
2024-04-25 18:21:06 +0200

    added unit test

--- projects/ngx-property-editor/src/lib/controller/pe-global-functions.spec.ts
+++ projects/ngx-property-editor/src/lib/controller/pe-global-functions.spec.ts
@@ -13,9 +13,13 @@ describe('PEGlobalFunctions', () => {
   });
 
   it('createDateUTC', () => {
-    const date: Date | undefined = PEGlobalFunctions.createDateUTC(2024, 3, 24, 13, 54, 7, 100);
+    let date: Date | undefined = PEGlobalFunctions.createDateUTC(2024, 3, 24, 13, 54, 7, 100);
     expect(date).toBeDefined();
     expect(date?.toISOString()).toEqual('2024-04-24T13:54:07.100Z');
+    
+    date = PEGlobalFunctions.createDateUTC(2024, 3, 22, 10, 1, 2);
+    expect(date).toBeDefined();
+    expect(date?.toISOString()).toEqual('2024-04-22T10:01:02.000Z');
   });
 
   const dataSource = [
