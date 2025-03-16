62a2a806db1831304720a22f362fea9a867e88e1
heinerwalter <heiner.walter@tu-dortmund.de>
2025-03-16 13:48:32 +0100

    Update country-input.component.spec.ts

--- projects/ngx-property-editor/src/lib/components/input/country-input/country-input.component.spec.ts
+++ projects/ngx-property-editor/src/lib/components/input/country-input/country-input.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CountryInputComponent } from './country-input.component';
 import { FormsModule } from '@angular/forms';
 import { FormGroupComponent } from '../form-group/form-group.component';
+import { SelectInputComponent } from '../select-input/select-input.component';
 
 describe('CountryInputComponent', () => {
   let component: CountryInputComponent;
@@ -13,6 +14,7 @@ describe('CountryInputComponent', () => {
       imports: [FormsModule],
       declarations: [
         CountryInputComponent,
+        SelectInputComponent,
         FormGroupComponent,
       ],
     })
