4cd3a74b3ea78b9daccf294f4be52a46026cd2aa
heinerwalter <heiner.walter@tu-dortmund.de>
2025-03-16 13:51:37 +0100

    Update modal.components.ts

--- projects/ngx-property-editor/src/lib/components/modal/modal.components.ts
+++ projects/ngx-property-editor/src/lib/components/modal/modal.components.ts
@@ -2,7 +2,7 @@ import { Type } from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
 import { AboutDialogComponent } from './about-dialog/about-dialog.component';
 
-export const iconComponents: Array<Type<any> | any[]> = [
+export const modalComponents: Array<Type<any> | any[]> = [
   ModalComponent,
   AboutDialogComponent,
 ];
