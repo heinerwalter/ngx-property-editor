4d770336bbac76bcdd212b6c48ec68cd4eb00a84
heinerwalter <heiner.walter@tu-dortmund.de>
2025-03-16 13:51:23 +0100

    Update components.ts

--- projects/ngx-property-editor/src/lib/components/components.ts
+++ projects/ngx-property-editor/src/lib/components/components.ts
@@ -1,12 +1,14 @@
 import { Type } from '@angular/core';
 import { bootstrapComponents } from './bootstrap/bootstrap.components';
-import { inputComponents } from './input/input.components';
-import { propertyViewComponents } from './property-views/property-views.components';
 import { iconComponents } from './icon/icon.components';
+import { inputComponents } from './input/input.components';
+import { modalComponents } from './modal/modal.components';
+import { propertyViewComponents } from './property-views/property-views.components';
 
 export const components: Array<Type<any> | any[]> = [
   ...bootstrapComponents,
   ...iconComponents,
   ...inputComponents,
+  ...modalComponents,
   ...propertyViewComponents,
 ];
