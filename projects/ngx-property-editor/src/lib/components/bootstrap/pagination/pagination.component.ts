d7a37cf36311c40166977ba55cf92fadebc96b29
heinerwalter <heiner.walter@tu-dortmund.de>
2025-04-06 10:24:19 +0000

    Replaced bootstrap pagination. close #22

--- projects/ngx-property-editor/src/lib/components/bootstrap/pagination/pagination.component.ts
+++ projects/ngx-property-editor/src/lib/components/bootstrap/pagination/pagination.component.ts
@@ -1,14 +1,19 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pe-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnChanges {
 
   /** Total number of entries.  */
   @Input() public collectionSize: number = 0;
+
+  /** Returns the total number of pages. */
+  protected get pageCount(): number {
+    return Math.ceil(this.collectionSize / this.pageSize);
+  }
 
   /** Index of the currently visible page starting with 0. */
   @Input() public page: number = 0;
@@ -18,6 +23,23 @@ export class PaginationComponent {
    * The index of the currently visible page is passed as a event argument.
    */
   @Output() public readonly pageChange: EventEmitter<number> = new EventEmitter<number>();
+
+  // region Visible Page Options
+
+  @Input() public showFirstAndLastPage: boolean = true;
+  @Input() public showPagesBeforeAndAfter: number = 2;
+
+  /**
+   * These pages are displayed as buttons in the pagination control.
+   * They include the current page, some pages before and after it,
+   * and the first and last page.
+   * If there is a gap between the first or last page and pages
+   * around the current page, an ellipsis is displayed inbetween.
+   * @see updateVisiblePageOptions
+   */
+  protected visiblePageOptions: (number | 'ellipsis')[] = [];
+
+  // endregion
 
   // region Page Size
 
@@ -41,20 +63,83 @@ export class PaginationComponent {
    * This array contains page sizes from which the user can choose one in a select input.
    * @see showPageSizeSelector
    */
-  @Input() public pageSizeOptions: number[] = [10];
+  @Input() public pageSizeOptions: number[] = [ 5, 10, 20, 50, 100 ];
 
   // endregion
 
   public constructor() {
   }
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('page')) {
+      this.page = this.fixPage(this.page);
+    }
+    if (changes.hasOwnProperty('collectionSize') ||
+        changes.hasOwnProperty('page') ||
+        changes.hasOwnProperty('pageSize')) {
+      this.updateVisiblePageOptions();
+    }
+  }
+
+  /**
+   * Updates the `visiblePageOptions` array.
+   */
+  private updateVisiblePageOptions(): void {
+    const visiblePageOptions: (number | 'ellipsis')[] = [];
+    const pageCount = this.pageCount;
+    const showPagesBeforeAndAfter = Math.max(this.showPagesBeforeAndAfter, 0);
+
+    // Determine the first and last visible page index
+    let firstPage = this.page - showPagesBeforeAndAfter;
+    let lastPage = this.page + showPagesBeforeAndAfter;
+    firstPage = this.fixPage(firstPage);
+    lastPage = this.fixPage(lastPage);
+    
+    // Add all page indices from first to last to the array
+    for (let i = firstPage; i <= lastPage; i++) {
+      visiblePageOptions.push(i);
+    }
+
+    // Add first and last pages, if showFirstAndLastPage == true
+    if (this.showFirstAndLastPage && !visiblePageOptions.includes(0)) {
+      if (firstPage >= 2)
+        visiblePageOptions.unshift('ellipsis');
+      visiblePageOptions.unshift(0);
+    }
+
+    if (this.showFirstAndLastPage && !visiblePageOptions.includes(pageCount - 1)) {
+      if (lastPage <= pageCount - 3)
+        visiblePageOptions.push('ellipsis');
+      visiblePageOptions.push(pageCount - 1);
+    }
+
+    // Assign new visiblePageOptions
+    this.visiblePageOptions = visiblePageOptions;
+  }
+
+  /**
+   * Fixes the given page index to fit into the interval [0, pageCount].
+   */
+  private fixPage(page: number): number {
+    if (page < 0) page = 0;
+    if (page >= this.pageCount) page = this.pageCount - 1;
+    return page;
+  }
+
   /**
    * Changes the visible page.
    * @param page The index of the new visible page starting with 0.
+   * @param updateVisiblePageOptions If true, `updateVisiblePageOptions()` is called after the change.
    */
-  public changePage(page: number): void {
+  public changePage(page: number, updateVisiblePageOptions: boolean = true): void {
+    page = this.fixPage(page);
+    if (page === this.page) return;
+
     this.page = page;
     this.pageChange.emit(this.page);
+
+    if (updateVisiblePageOptions)
+      this.updateVisiblePageOptions();
   }
 
   /**
@@ -62,8 +147,12 @@ export class PaginationComponent {
    * @param pageSize The new number of entries per page.
    */
   public changePageSize(pageSize: number): void {
+    if (pageSize === this.pageSize) return;
+
     this.pageSize = pageSize;
     this.pageSizeChange.emit(this.pageSize);
+    this.changePage(this.page, false);
+    this.updateVisiblePageOptions();
   }
 
 }
